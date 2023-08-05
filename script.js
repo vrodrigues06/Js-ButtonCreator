4b9384896b0a631c15bba44963d72bd12c5c8189
V.Rodrigues <vitorrodrigues06@outlook.com>
2023-08-05 10:57:52 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+const controles = document.forms.controles;
+const css = document.querySelector(".css");
+const btn = document.querySelector(".btn");
+
+const handleStyle = {
+  element: btn,
+  texto(valor) {
+    btn.innerText = valor;
+  },
+  color(valor) {
+    btn.style.color = valor;
+  },
+  backgroundColor(valor) {
+    btn.style.backgroundColor = valor;
+  },
+  height(valor) {
+    btn.style.height = valor + "px";
+  },
+  width(valor) {
+    btn.style.width = valor + "px";
+  },
+  border(valor) {
+    btn.style.border = valor;
+  },
+  borderRadius(valor) {
+    btn.style.borderRadius = valor + "px";
+  },
+  fontSize(valor) {
+    btn.style.fontSize = valor + "px";
+  },
+  fontFamily(valor) {
+    btn.style.fontFamily = valor;
+  },
+  fontWeight(valor) {
+    btn.style.fontWeight = valor;
+  },
+};
+function handleChange(event) {
+  const target = event.target;
+  const name = target.name;
+  const valor = target.value;
+  handleStyle[name](valor);
+  showCss();
+}
+controles.addEventListener("change", handleChange);
+
+function showCss() {
+  css.innerHTML =
+    "<span>" + btn.style.cssText.split("; ").join(";<span></span>");
+}
